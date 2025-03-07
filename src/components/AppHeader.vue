59aa42aa8fec6b272bdfe60e14620c1d43a5d200
RaulREscobar <tradingappdevelopment@outlook.com>
2025-03-06 23:58:21 -0300

    added routes in menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,19 +1,21 @@
 <script setup>
 
-const links = [
+const itemnsMenu = [
     'Proyectos',
-    'C.V.',
+    'CV',
     'Contactame',
 ]
 </script>
 
 <template>
     <v-toolbar theme="light" fixed="true">
-        <v-img :width="50" aspect-ratio="1/1" cover
+        <v-btn href="/">
+            <v-img :width="50" aspect-ratio="1/1" cover
             src="@/../public/logoBlack.png"></v-img>
+        </v-btn>
         <v-spacer></v-spacer>
-        <v-btn v-for="link in links" :key="link">
-            {{ link }}
+        <v-btn v-for="item in itemnsMenu" :key="item" :href="item" >
+            {{ item }}
         </v-btn>
     </v-toolbar>
 </template>
---
+++ src/pages/CV.vue
@@ -0,0 +1,3 @@
+<template>
+    Página CV
+</template>
