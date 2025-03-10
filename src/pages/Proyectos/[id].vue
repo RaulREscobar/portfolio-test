066f17599788d3bd78eba10d6c388bd844f62bf5
RaulREscobar <tradingappdevelopment@outlook.com>
2025-03-09 23:13:27 -0300

    added proyects

--- src/pages/Proyectos/[id].vue
+++ src/pages/Proyectos/[id].vue
@@ -1,5 +1,9 @@
 <script setup>
 import { ref } from 'vue'
+import imgChongazen from '@/assets/chongazen.png';
+import imgAtym from '@/assets/atym.jpg';
+import imgPaula from '@/assets/paula.png'
+
 
 definePageMeta({
     layout: "proyectos"
@@ -10,14 +14,38 @@ const show = ref(false)
 const proyects = [
     {
         name: "ChongaZen",
-        category:"Sitio web Wordpress",
-        description:"ChongaZen es un espacio de autoconocimiento y encuentro interior dirigido por Mecha Garrido, que ofrece servicios de astrología y tarot en Madrid, España. A través de consultas personalizadas como cartas astrales, revoluciones solares y tarot astrológico, busca brindar a sus clientes herramientas para comprender y sanar aspectos de sus vidas",
+        category: "Sitio web Wordpress",
+        description: "ChongaZen es un espacio de autoconocimiento y encuentro interior dirigido por Mecha Garrido. A través de consultas personalizadas como cartas astrales, revoluciones solares y tarot astrológico, busca brindar a sus clientes herramientas para comprender y sanar aspectos de sus vidas",
         colorBtn: "orange-lighten-2",
-        url:"https://chongazen.com",
-        img:""
-    }
+        url: "https://chongazen.com",
+        img: imgChongazen,
+    },
+    {
+        name: "Atym",
+        category: "Sitio web Wordpress",
+        description: "Atym.online es un estudio de diseño gráfico que se especializa en la creación de identidades visuales, marcas e historias. Ofrecen servicios de diseño de branding y consultorías personales, acompañando a sus clientes en el desarrollo de su mejor versión gráfica.",
+        colorBtn: "light-blue-lighten-2",
+        url: "https://atym.online",
+        img: imgAtym,
+    },
+    {
+        name: "Paula Aranoa",
+        category: "Sitio web Wordpress",
+        description: "El sitio web paulaaranoa.com es un portafolio fotográfico donde la artista Paula Aranoa exhibe su trabajo visual. Presenta diversas series de fotografía artística, explorando temas como la introspección, lo cotidiano, la arquitectura y el paisaje.",
+        colorBtn: "",
+        url: "https://paulaaranoa.com",
+        img: imgPaula,
+    },
+    {
+        name: "Lamparas DeLaLuz",
+        category: "Sitio web Wordpress",
+        description: "El sitio web lamparasdelaluz.com es una tienda online de iluminación artesanal. Se especializa en la venta de lámparas de diseño, ofreciendo una variedad de productos como lámparas colgantes, de pie, de mesa y apliques para interiores y exteriores.",
+        colorBtn: "brown-lighten-2",
+        url: "https://lamparasdelaluz.com",
+        img: imgPaula,
+    },
 ]
-</script>
+</script> 
 
 <template>
     <v-conteiner>
@@ -25,8 +53,10 @@ const proyects = [
             Mis sitios realizados en
             {{ route.params.id }}
         </h1>
-        <v-card v-for="item of proyects" :key=item.name class="mx-auto" max-width="344">
-            <v-img height="200px" src="@../../../public/foto-site.jpg" cover></v-img>
+
+        <v-row class="mt-2">
+            <v-card v-for="item of proyects" :key=item.name class="mx-auto" max-width="344" width="250px">
+            <v-img height="200px" :src=item.img ></v-img>
 
             <v-card-title>
                 {{ item.name }}
@@ -37,7 +67,7 @@ const proyects = [
             </v-card-subtitle>
 
             <v-card-actions>
-                <v-btn :href=item.url  target="_blank" :color=item.colorBtn text="Ir al sitio"></v-btn>
+                <v-btn :href=item.url target="_blank" :color=item.colorBtn text="Ir al sitio"></v-btn>
 
                 <v-spacer></v-spacer>
 
@@ -54,6 +84,8 @@ const proyects = [
                 </div>
             </v-expand-transition>
         </v-card>
+        </v-row>
+      
     </v-conteiner>
 
 </template>
